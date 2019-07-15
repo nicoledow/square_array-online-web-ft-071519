a154ca4ea52d19fe601d789da6f616485d6d5956
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 16:52:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   squared = []
   
-  array.each do {|element| squared << (element * element)}
+  #array.each do {|element| squared << (element * element)}
+  
+  array.each do |element|
+    squared << (element * element)
+  end
 end
